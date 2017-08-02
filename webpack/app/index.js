eecc7750c1e3203db73b815da28131597a0ca8da
Park Sung Min <vamf12@gmail.com>
2017-08-02 18:43:59 +0900

    main javascript 경로 변경

--- webpack/app/index.js
+++ webpack/app/index.js
@@ -1,3 +1,3 @@
 require('bootstrap/dist/css/bootstrap.css');
-var kt = require('../../build/classes/main/html2kotlin');
+var kt = require('html2kotlin');
 kt.com.kotlin.everywhere.html2kotlin.main_kand9s$(['#app']);
